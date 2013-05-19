855995fa5896ff9aa9dd2f72733235f878090b1d
silverwind <silv3rwind@gmail.com>
2013-05-20 01:22:09 +0200

    Optimize Regex and formatting

--- core.lua
+++ core.lua
@@ -1,45 +1,45 @@
 local select, UnitBuff, UnitDebuff, UnitAura, tonumber, strfind, hooksecurefunc =
-	select, UnitBuff, UnitDebuff, UnitAura, tonumber, strfind, hooksecurefunc
+    select, UnitBuff, UnitDebuff, UnitAura, tonumber, strfind, hooksecurefunc
 
-local function addLine(self,id,isItem)
-	if isItem then
-		self:AddDoubleLine("ItemID:","|cffffffff"..id)
-	else
-		self:AddDoubleLine("SpellID:","|cffffffff"..id)
-	end
-	self:Show()
+local function addLine(self, id, isItem)
+    if isItem then
+        self:AddDoubleLine("ItemID:", "|cffffffff" .. id)
+    else
+        self:AddDoubleLine("SpellID:", "|cffffffff" .. id)
+    end
+    self:Show()
 end
 
-hooksecurefunc(GameTooltip, "SetUnitBuff", function(self,...)
-	local id = select(11,UnitBuff(...))
-	if id then addLine(self,id) end
+hooksecurefunc(GameTooltip, "SetUnitBuff", function(self, ...)
+    local id = select(11, UnitBuff(...))
+    if id then addLine(self, id) end
 end)
 
-hooksecurefunc(GameTooltip, "SetUnitDebuff", function(self,...)
-	local id = select(11,UnitDebuff(...))
-	if id then addLine(self,id) end
+hooksecurefunc(GameTooltip, "SetUnitDebuff", function(self, ...)
+    local id = select(11, UnitDebuff(...))
+    if id then addLine(self, id) end
 end)
 
-hooksecurefunc(GameTooltip, "SetUnitAura", function(self,...)
-	local id = select(11,UnitAura(...))
-	if id then addLine(self,id) end
+hooksecurefunc(GameTooltip, "SetUnitAura", function(self, ...)
+    local id = select(11, UnitAura(...))
+    if id then addLine(self, id) end
 end)
 
 GameTooltip:HookScript("OnTooltipSetSpell", function(self)
-	local id = select(3,self:GetSpell())
-	if id then addLine(self,id) end
+    local id = select(3, self:GetSpell())
+    if id then addLine(self, id) end
 end)
 
 hooksecurefunc("SetItemRef", function(link, ...)
-	local id = tonumber(link:match("spell:(%d+)"))
-	if id then addLine(ItemRefTooltip,id) end
+    local id = tonumber(link:match("spell:(%d+)"))
+    if id then addLine(ItemRefTooltip, id) end
 end)
 
 local function attachItemTooltip(self)
-	local link = select(2,self:GetItem())
-	if not link then return end
-	local id = select(3,strfind(link, "^|%x+|Hitem:(%-?%d+):(%d+):(%d+):(%d+):(%d+):(%d+):(%-?%d+):(%-?%d+)"))
-	if id then addLine(self,id,true) end
+    local link = select(2, self:GetItem())
+    if not link then return end
+    local id = select(3, strfind(link, "^|%x+|Hitem:(%-?%d+)"))
+    if id then addLine(self, id, true) end
 end
 
 GameTooltip:HookScript("OnTooltipSetItem", attachItemTooltip)
