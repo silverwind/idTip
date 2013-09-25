45bf2db0a2db7b168dc963b28cc917e7defdd8ea
silverwind <silv3rwind@gmail.com>
2013-09-25 18:24:45 +0200

    Add NPC ID support

--- core.lua
+++ core.lua
@@ -1,45 +1,61 @@
-local select, UnitBuff, UnitDebuff, UnitAura, tonumber, strfind, hooksecurefunc =
-    select, UnitBuff, UnitDebuff, UnitAura, tonumber, strfind, hooksecurefunc
+local select, UnitBuff, UnitDebuff, UnitAura, UnitGUID, tonumber, strfind, hooksecurefunc =
+    select, UnitBuff, UnitDebuff, UnitAura, UnitGUID, tonumber, strfind, hooksecurefunc
 
-local function addLine(self, id, isItem)
-    if isItem then
-        self:AddDoubleLine("ItemID:", "|cffffffff" .. id)
-    else
-        self:AddDoubleLine("SpellID:", "|cffffffff" .. id)
-    end
+local types = {
+    spell = "SpellID",
+    item  = "ItemID",
+    unit  = "NPC ID"
+}
+
+local function addLine(self,id,type)
+    self:AddDoubleLine(type..":","|cffffffff"..id)
     self:Show()
 end
 
-hooksecurefunc(GameTooltip, "SetUnitBuff", function(self, ...)
-    local id = select(11, UnitBuff(...))
-    if id then addLine(self, id) end
+-- Spells
+hooksecurefunc(GameTooltip, "SetUnitBuff", function(self,...)
+    local id = select(11,UnitBuff(...))
+    if id then addLine(self, id, types.spell) end
 end)
 
-hooksecurefunc(GameTooltip, "SetUnitDebuff", function(self, ...)
-    local id = select(11, UnitDebuff(...))
-    if id then addLine(self, id) end
+hooksecurefunc(GameTooltip, "SetUnitDebuff", function(self,...)
+    local id = select(11,UnitDebuff(...))
+    if id then addLine(self,id, types.spell) end
 end)
 
-hooksecurefunc(GameTooltip, "SetUnitAura", function(self, ...)
-    local id = select(11, UnitAura(...))
-    if id then addLine(self, id) end
+hooksecurefunc(GameTooltip, "SetUnitAura", function(self,...)
+    local id = select(11,UnitAura(...))
+    if id then addLine(self, id, types.spell) end
 end)
 
 GameTooltip:HookScript("OnTooltipSetSpell", function(self)
-    local id = select(3, self:GetSpell())
-    if id then addLine(self, id) end
+    local id = select(3,self:GetSpell())
+    if id then addLine(self, id, types.spell) end
 end)
 
+-- Units
+local f = CreateFrame("frame")
+f:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
+f:SetScript("OnEvent", function()
+    if GameTooltip:IsVisible() and not UnitIsPlayer("mouseover") then
+        local id = tonumber(UnitGUID("mouseover"):sub(6, 10), 16)
+        if id ~= 0 then
+            addLine(GameTooltip, id, types.unit);
+        end
+    end
+end)
+
+-- Items
 hooksecurefunc("SetItemRef", function(link, ...)
     local id = tonumber(link:match("spell:(%d+)"))
-    if id then addLine(ItemRefTooltip, id) end
+    if id then addLine(ItemRefTooltip, id, types.item) end
 end)
 
 local function attachItemTooltip(self)
-    local link = select(2, self:GetItem())
+    local link = select(2,self:GetItem())
     if not link then return end
-    local id = select(3, strfind(link, "^|%x+|Hitem:(%-?%d+)"))
-    if id then addLine(self, id, true) end
+    local id = select(3,strfind(link, "^|%x+|Hitem:(%-?%d+):(%d+):(%d+):(%d+):(%d+):(%d+):(%-?%d+):(%-?%d+)"))
+    if id then addLine(self, id, types.item) end
 end
 
 GameTooltip:HookScript("OnTooltipSetItem", attachItemTooltip)
