1ca894f62a35807d23bc619435de4e1d79729674
silverwind <me@silverwind.io>
2024-06-17 00:18:29 +0200

    refactor

--- idTip.lua
+++ idTip.lua
@@ -341,37 +341,38 @@ hookScript(ItemRefShoppingTooltip2, "OnTooltipSetItem", attachItemTooltip)
 hookScript(ShoppingTooltip1, "OnTooltipSetItem", attachItemTooltip)
 hookScript(ShoppingTooltip2, "OnTooltipSetItem", attachItemTooltip)
 
+local function achievementOnEnter(button)
+  GameTooltip:SetOwner(button, "ANCHOR_NONE")
+  GameTooltip:SetPoint("TOPLEFT", button, "TOPRIGHT", 0, 0)
+  addLine(GameTooltip, button.id, kinds.achievement)
+  GameTooltip:Show()
+end
+
+local function criteriaOnEnter(index)
+  return function(self)
+    local button = self:GetParent() and self:GetParent():GetParent()
+    if not button or not button.id then return end
+    local criteriaid = select(10, GetAchievementCriteriaInfo(button.id, self.___index or index))
+    if criteriaid then
+      if not GameTooltip:IsVisible() then
+        GameTooltip:SetOwner(button:GetParent(), "ANCHOR_NONE")
+      end
+      GameTooltip:SetPoint("TOPLEFT", button, "TOPRIGHT", 0, 0)
+      addLine(GameTooltip, button.id, kinds.achievement)
+      addLine(GameTooltip, criteriaid, kinds.criteria)
+      GameTooltip:Show()
+    end
+  end
+end
+
 -- Achievement Frame Tooltips
 local f = CreateFrame("frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(_, _, what)
   if what == "Blizzard_AchievementUI" then
-    -- common functions between branches
-    local achievementEnter = function(button)
-      GameTooltip:SetOwner(button, "ANCHOR_NONE")
-      GameTooltip:SetPoint("TOPLEFT", button, "TOPRIGHT", 0, 0)
-      addLine(GameTooltip, button.id, kinds.achievement)
-      GameTooltip:Show()
-    end
-    local criteriaEnter = function(index)
-      return function(self)
-        local button = self:GetParent() and self:GetParent():GetParent()
-        if not button or not button.id then return end
-        local criteriaid = select(10, GetAchievementCriteriaInfo(button.id, self.___index or index))
-        if criteriaid then
-          if not GameTooltip:IsVisible() then
-            GameTooltip:SetOwner(button:GetParent(), "ANCHOR_NONE")
-          end
-          GameTooltip:SetPoint("TOPLEFT", button, "TOPRIGHT", 0, 0)
-          addLine(GameTooltip, button.id, kinds.achievement)
-          addLine(GameTooltip, criteriaid, kinds.criteria)
-          GameTooltip:Show()
-        end
-      end
-    end
     if AchievementTemplateMixin then
       -- dragonflight
-      hook(AchievementTemplateMixin, "OnEnter", achievementEnter)
+      hook(AchievementTemplateMixin, "OnEnter", achievementOnEnter)
       hook(AchievementTemplateMixin, "OnLeave", GameTooltip_Hide)
 
       local hooked = {}
@@ -380,7 +381,7 @@ f:SetScript("OnEvent", function(_, _, what)
           local frame = self[pool][index]
           frame.___index = index
           if frame and not hooked[frame] then
-            hookScript(frame, "OnEnter", criteriaEnter(index))
+            hookScript(frame, "OnEnter", criteriaOnEnter(index))
             hookScript(frame, "OnLeave", GameTooltip_Hide)
             hooked[frame] = true
           end
@@ -393,14 +394,14 @@ f:SetScript("OnEvent", function(_, _, what)
     elseif AchievementFrameAchievementsContainer then
       -- pre-dragonflight
       for i,button in ipairs(AchievementFrameAchievementsContainer.buttons) do
-        hookScript(button, "OnEnter", achievementEnter)
+        hookScript(button, "OnEnter", achievementOnEnter)
         hookScript(button, "OnLeave", GameTooltip_Hide)
 
         local hooked = {}
         hook(_G, "AchievementButton_GetCriteria", function(index, renderOffScreen)
           local frame = _G["AchievementFrameCriteria" .. (renderOffScreen and "OffScreen" or "") .. index]
           if frame and not hooked[frame] then
-            hookScript(frame, "OnEnter", criteriaEnter(index))
+            hookScript(frame, "OnEnter", criteriaOnEnter(index))
             hookScript(frame, "OnLeave", GameTooltip_Hide)
             hooked[frame] = true
           end
