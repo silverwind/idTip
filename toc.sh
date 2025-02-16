475b0f6d7bee65e30b258cfd610cb50b4603f4bd
silverwind <me@silverwind.io>
2025-02-16 12:21:33 +0100

    validate version string in toc.sh

--- toc.sh
+++ toc.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 function toc {
-  local version="$(nc "us.version.battle.net" 1119 <<< "v1/products/$1/versions" | awk -F'|' '/^us/{print $6}')"
+  local version="$(nc us.version.battle.net 1119 <<< "v1/products/$1/versions" | awk -F"|" '/^us/{print $6}')"
   version="${version%.*}"
   if [[ "$version" == 1.* ]]; then
     version="${version/./}"
@@ -21,4 +21,10 @@ VERSION_STRING="$(echo -e \
   "$(toc "wow_classic_era_ptr")\n" \
   | sort -n | uniq | xargs | perl -p -e 's# #, #g')"
 
-perl -p -i -e "s|## Interface: .+|## Interface: $VERSION_STRING|" idTip.toc
+if [[ "$VERSION_STRING" =~ ^[0-9,\ ]+$ ]]; then
+  perl -p -i -e "s|## Interface: .+|## Interface: $VERSION_STRING|" idTip.toc
+  exit 0
+else
+  echo "'""$VERSION_STRING""' does not match expected format"
+  exit 1
+fi
